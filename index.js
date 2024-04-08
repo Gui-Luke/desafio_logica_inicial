c7cdb0f5ebe91e70f961625596601579b2a6abea
Gui-Luke <gui.lucas.dev@gmail.com>
2024-04-07 21:53:26 -0300

    Concluindo meu primeiro desafio

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-let recep = "Olá, Mundão"
+let nomeDoHeroi = "Kratos"
+let nivelDoHeroi = "15000"
 
-console.log(recep)
+switch(true) {
+    case nivelDoHeroi < 1000:
+        console.log("Ferro")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+    
+    case nivelDoHeroi > 1001 && nivelDoHeroi <2000:
+        console.log("Bronze")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
 
-console.log("Adicionando outro texto")
+    case nivelDoHeroi > 2001 && nivelDoHeroi <5000:
+        console.log("Prata")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+    
+    case nivelDoHeroi >5001 && nivelDoHeroi <7000:
+        console.log("Ouro")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+
+    case nivelDoHeroi >7001 && nivelDoHeroi <8000:
+        console.log("Platina")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+
+    case nivelDoHeroi >8001 && nivelDoHeroi <9000:
+        console.log("Ascendente")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+
+    case nivelDoHeroi >9001 && nivelDoHeroi <10000:
+        console.log("Imortal")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+        break
+
+    default:
+        console.log("Radiante")
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi)
+}
+
